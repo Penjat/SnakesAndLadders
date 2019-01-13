900656d41291e727a3da1d510d170b06e1e5c3c2
Penjat <theSpencerSymington@Gmail.com>
2019-01-13 15:36:08 -0800

    Initial Commit

---
+++ SnakesAndLadders/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SnakesAndLadders
+//
+//  Created by Spencer Symington on 2019-01-13.
+//  Copyright © 2019 Spencer Symington. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
